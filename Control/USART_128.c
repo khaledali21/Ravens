aca374cbbf0aa6297df4463564c3c70ed824b429
moriarty21 <khaled21.ali33@gmail.com>
2020-02-07 20:19:17 +0200

    adding USART files, removing UTILS_128.h

---
+++ Control/USART_128.c
@@ -0,0 +1,177 @@
+/*
+ * USART.c
+ *
+ *  Created on: 6 Oct 2018
+ *      Author: mahmoud
+ */
+#include "USART_2560.h"
+/*
+ * FUNCTION TO US :
+ * UART_sendByte(DATA) send byte
+ * UART_receiveByte()  receive byte
+ * baud rate = 9600 symbol/second
+ * UART_sendString(" ... #") NOTE : you must send '#' at the end of the string
+ * UART_receiveString()
+ *
+ */
+void UART0_init()
+{
+	//UCSR0A=(1<<U2X);											// double speed mode
+
+	UCSR0B	=	0b00011000;									//sender and receiver
+	UCSR0C	= 	0b10000110;							//odd parity check and send 8 bits
+
+	UBRR0L=103;
+
+}
+
+void UART1_init()
+{
+	//UCSR1A=(1<<U2X);											// double speed mode
+	UCSR1B	=	0b00011000;										//sender and receiver
+	UCSR1C	= 	0b10000110;							//odd parity check and send 8 bits
+	//UBRR1H=BAUD_PRESCALE<<8; 								//baud rate 9600
+	UBRR1L	=	103;
+
+}
+
+
+void UART2_init()
+{
+	//UCSR2A	=	(1<<U2X);											// double speed mode
+	UCSR2B	= 	0b00011000;									//sender and receiver
+	UCSR2C	=	0b10000110;							//odd parity check and send 8 bits
+	//UBRR2H=BAUD_PRESCALE<<8; 								//baud rate 9600
+	UBRR2L=103;
+}
+
+
+
+/* this fun sends a byte and waits till the U DATA is sent then send another byte  to avoid overwriting  */
+void UART0_sendByte (u8 data)
+{
+	UDR0=data;
+	while (!GET_BIT(UCSR0A,5)){}
+	SET_BIT(UCSR0A,TXC);
+
+}
+/* this fun receive a byte and waits until the data is empty to avoid overwriting */
+u8 UART0_receiveByte (void)
+{
+	while (!GET_BIT(UCSR0A,7)){}
+	/*
+	 * wait until the last byte is received
+	 */
+	return UDR0;
+}
+
+/* this fun sends a byte and waits till the U DATA is sent then send another byte  to avoid overwriting  */
+void UART1_sendByte (u8 data)
+{
+	UDR1=data;
+	while (!GET_BIT(UCSR1A,5)){}
+	SET_BIT(UCSR1A,TXC);
+
+}
+/* this fun receive a byte and waits untill the data is empty to avoid overwriting */
+u8 UART1_receiveByte (void)
+{
+	while (!GET_BIT(UCSR1A,7)){}
+	/*
+	 * wait until the last byte is received
+	 */
+	return UDR1;
+}
+
+
+/* this fun sends a byte and waits till the U DATA is sent then send another byte  to avoid overwriting  */
+void UART2_sendByte (u8 data)
+{
+	UDR2=data;
+	while (!GET_BIT(UCSR2A,5)){}
+	SET_BIT(UCSR2A,TXC);
+
+}
+/* this fun receive a byte and waits untill the data is empty to avoid overwriting */
+u8 UART2_receiveByte (void)
+{
+	while (!GET_BIT(UCSR2A,7)){}
+	/*
+	 * wait until the last byte is received
+	 */
+	return UDR2;
+}
+
+
+
+void UART0_sendString (const u8 *str)
+{
+	while (*str != '\0')
+	{
+		UART0_sendByte(*str);
+		str++;
+	}
+}
+
+
+void UART0_receiveString (u8 *str)
+{
+	*str=UART0_receiveByte();
+	while (*str != '#')
+	{
+		str++;
+		*str=UART0_receiveByte();
+
+	}
+	*str='\0';
+
+}
+
+
+void UART1_sendString (const u8 *str)
+{
+	while (*str != '\0')
+	{
+		UART1_sendByte(*str);
+		str++;
+	}
+}
+
+
+void UART1_receiveString (u8 *str)
+{
+	*str=UART1_receiveByte();
+	while (*str != '#')
+	{
+		str++;
+		*str=UART1_receiveByte();
+
+	}
+	*str='\0';
+
+}
+
+
+
+void UART2_sendString (const u8 *str)
+{
+	while (*str != '\0')
+	{
+		UART2_sendByte(*str);
+		str++;
+	}
+}
+
+
+void UART2_receiveString (u8 *str)
+{
+	*str=UART2_receiveByte();
+	while (*str != ',')
+	{
+		str++;
+		*str=UART2_receiveByte();
+
+	}
+	*str='\0';
+
+}
---
+++ Control/USART_128.h
@@ -0,0 +1,72 @@
+/*
+ * USART.h
+ *
+ *  Created on: 6 Oct 2018
+ *      Author: mahmoud
+ */
+
+#ifndef USART_H_
+#define USART_H_
+
+#include"STD_TYPES.h"
+#include"UTILS.h"
+//#include"micro_config.h"
+/* FUNCTION DECLARATIONS                */
+void UART0_init();
+void UART1_init();
+void UART2_init();
+u8 UART0_receiveByte (void);
+void UART0_sendByte(u8 data);
+u8 UART1_receiveByte (void);
+void UART1_sendByte(u8 data);
+u8 UART2_receiveByte (void);
+void UART2_sendByte(u8 data);
+void UART0_receiveString (u8 *str);
+void UART0_sendString (const u8 *str);
+void UART1_receiveString (u8 *str);
+void UART1_sendString (const u8 *str);
+void UART2_receiveString (u8 *str);
+void UART2_sendString (const u8 *str);
+
+#define UCSR0A  (* (volatile u8 *) (0xC0) )
+#define UCSR0B  (* (volatile u8 *) (0xC1) )
+#define UCSR0C  (* (volatile u8 *) (0xC2) )
+#define UBRR0H  (* (volatile u8 *) (0xC5) )
+#define UBRR0L  (* (volatile u8 *) (0xC4) )
+#define UDR0    (* (volatile u8 *) (0xC6) )
+
+
+#define UCSR1A  (* (volatile u8 *) (0xC8) )
+#define UCSR1B  (* (volatile u8 *) (0xC9) )
+#define UCSR1C  (* (volatile u8 *) (0xCA) )
+#define UBRR1H  (* (volatile u8 *) (0xCD) )
+#define UBRR1L  (* (volatile u8 *) (0xCC) )
+#define UDR1    (* (volatile u8 *) (0xCE) )
+
+
+
+#define UCSR2A  (* (volatile u8 *) (0xD0) )
+#define UCSR2B  (* (volatile u8 *) (0xD1) )
+#define UCSR2C  (* (volatile u8 *) (0xD2) )
+#define UBRR2H  (* (volatile u8 *) (0xD5) )
+#define UBRR2L  (* (volatile u8 *) (0xD4) )
+#define UDR2    (* (volatile u8 *) (0xD6) )
+
+
+
+#define U2X		1
+#define RXEN	4
+#define TXEN	3
+#define UPM0	4
+#define UPM1	5
+#define UCSZ0	1
+#define UCSZ1	2
+#define TXC		6
+#define RXC		7
+
+
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE 51
+
+
+#endif /* USART_H_ */
