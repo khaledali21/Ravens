8b4294eb0dbd2d2362de80d1ebcbd88c43afe9e7
Yousef <yousef.ahmed97@gmail.com>
2020-02-07 20:52:08 +0300

    adding mpu files

---
+++ Control/DDIO_128.c
@@ -0,0 +1,174 @@
+/*
+ * DDIO_2560.c
+ *
+ *  Created on: Oct 6, 2018
+ *      Author: Ahmed Anwar
+ */
+
+
+#include "DDIO_128.h"
+#include "STD_TYPES_128.h"
+#include "UTILS_128.h"
+void SetPinDir(u8 PinNumber,u8 PinDirection)
+{
+	u8 PortLetter = PinNumber/8;
+
+	switch(PortLetter){
+		case (0):
+			if(PinDirection)
+			SET_BIT(DDRA,PinNumber%8);
+			else
+			CLEAR_BIT(DDRA,PinNumber%8);
+			break;
+
+		case (1):
+		    if(PinDirection)
+			SET_BIT(DDRB,PinNumber%8);
+	        else
+			CLEAR_BIT(DDRB,PinNumber%8);
+			break;
+
+		case  (2):
+			if(PinDirection)
+			SET_BIT(DDRC,PinNumber%8);
+			else
+			CLEAR_BIT(DDRC,PinNumber%8);
+            break;
+
+		case (3):
+			if(PinDirection)
+			SET_BIT(DDRD,PinNumber%8);
+			else
+			CLEAR_BIT(DDRD,PinNumber%8);
+			break;
+
+		case (4):
+			if(PinDirection)
+			SET_BIT(DDRE,PinNumber%8);
+			else
+			CLEAR_BIT(DDRE,PinNumber%8);
+			break;
+
+		case (5):
+			if(PinDirection)
+			SET_BIT(DDRF,PinNumber%8);
+			else
+			CLEAR_BIT(DDRF,PinNumber%8);
+			break;
+
+		case (6):
+			if(PinDirection)
+			SET_BIT(DDRG,PinNumber%8);
+			else
+			CLEAR_BIT(DDRG,PinNumber%8);
+			break;
+
+
+	}
+}
+
+
+
+
+
+void SetPinVal(u8 PinNumber,u8 PinValue)
+{
+	u8 PortLetter = PinNumber/8;
+
+	switch(PortLetter){
+		case (0):
+		   if(PinValue)
+			   SET_BIT(PORTA,PinNumber%8);
+		   else
+			   CLEAR_BIT(PORTA,PinNumber%8);
+				break;
+
+		case (1):
+			if(PinValue)
+			    SET_BIT(PORTB,PinNumber%8);
+			else
+			    CLEAR_BIT(PORTB,PinNumber%8);
+				break;
+
+		case (2):
+			if(PinValue)
+				SET_BIT(PORTC,PinNumber%8);
+			else
+				CLEAR_BIT(PORTC,PinNumber%8);
+         	 	break;
+
+		case (3):
+			if(PinValue)
+				SET_BIT(PORTD,PinNumber%8);
+			else
+				CLEAR_BIT(PORTD,PinNumber%8);
+        		break;
+
+		case (4):
+			if(PinValue)
+				SET_BIT(PORTE,PinNumber%8);
+			else
+				CLEAR_BIT(PORTE,PinNumber%8);
+        		break;
+
+		case (5):
+			if(PinValue)
+				SET_BIT(PORTF,PinNumber%8);
+			else
+				CLEAR_BIT(PORTF,PinNumber%8);
+				break;
+
+		case (6):
+			if(PinValue)
+				SET_BIT(PORTG,PinNumber%8);
+			else
+				CLEAR_BIT(PORTG,PinNumber%8);
+        		break;
+
+
+	}
+
+}
+
+u8 ReadPinVal(u8 PinNumber)
+{
+	u8 PortLetter = PinNumber/8;
+
+	switch(PortLetter)
+	{
+		case (0):
+		return (GET_BIT(PINA,PinNumber%8));
+		break;
+		case (1):
+		return (GET_BIT(PINB,PinNumber%8));
+		break;
+		case (2):
+		return (GET_BIT(PINC,PinNumber%8));
+		break;
+		case (3):
+		return (GET_BIT(PIND,PinNumber%8));
+		break;
+		case (4):
+		return (GET_BIT(PINE,PinNumber%8));
+		break;
+		case (5):
+		return (GET_BIT(PINF,PinNumber%8));
+		break;
+		case (6):
+		return (GET_BIT(PING,PinNumber%8));
+		break;
+
+	}
+
+}
+
+
+
+
+	}
+
+}
+
+
+
+
---
+++ Control/DDIO_128.h
@@ -0,0 +1,142 @@
+
+
+#ifndef DDIO_128_H_
+#define DDIO_128_H_
+
+
+#include "STD_TYPES_128.h"
+#include "UTILS_128.h"
+
+/*
+ * Define ATMEGA128 Registers
+ */
+
+/* PORT A Registers */
+#define PINA  (* (volatile u8 *) ( 0x39) )
+#define DDRA  (* (volatile u8 *) ( 0x3A) )
+#define PORTA (* (volatile u8 *) ( 0x3B) )
+
+
+/* PORT B Registers */
+#define PINB (* (volatile u8 *) ( 0x36) )
+#define DDRB  (* (volatile u8 *) ( 0x37) )
+#define PORTB (* (volatile u8 *) ( 0x38) )
+
+/* PORT C Registers */
+#define PINC  (* (volatile u8 *) ( 0x33) )
+#define DDRC  (* (volatile u8 *) ( 0x34) )
+#define PORTC (* (volatile u8 *) ( 0x35) )
+
+/* PORT D Registers */
+#define PIND  (* (volatile u8 *) ( 0x30) )
+#define DDRD  (* (volatile u8 *) ( 0x31) )
+#define PORTD (* (volatile u8 *) ( 0x32) )
+
+/* PORT E Registers */
+#define PINE  (* (volatile u8 *) ( 0x23) )
+#define DDRE  (* (volatile u8 *) ( 0x22) )
+#define PORTE (* (volatile u8 *) ( 0x2E) )
+
+/* PORT F Registers */
+#define PINF  (* (volatile u8 *) ( 0x20) )
+#define DDRF  (* (volatile u8 *) ( 0x61) )
+#define PORTF (* (volatile u8 *) ( 0x62) )
+
+/* PORT G Registers */
+#define PING  (* (volatile u8 *) ( 0x63) )
+#define DDRG  (* (volatile u8 *) ( 0x64) )
+#define PORTG (* (volatile u8 *) ( 0x65) )
+
+
+
+
+#define INPUT_PULL_UP	    0
+#define INPUT_FLOAT         2
+#define OUTPUT				1
+
+#define LOW					0
+#define HIGH				1
+
+
+
+
+#define PINA_0 0
+#define PINA_1 1
+#define PINA_2 2
+#define PINA_3 3
+#define PINA_4 4
+#define PINA_5 5
+#define PINA_6 6
+#define PINA_7 7
+
+
+#define PINB_0 8
+#define PINB_1 9
+#define PINB_2 10
+#define PINB_3 11
+#define PINB_4 12
+#define PINB_5 13
+#define PINB_6 14
+#define PINB_7 15
+
+
+#define PINC_0 16
+#define PINC_1 17
+#define PINC_2 18
+#define PINC_3 19
+#define PINC_4 20
+#define PINC_5 21
+#define PINC_6 22
+#define PINC_7 23
+
+
+#define PIND_0 24
+#define PIND_1 25
+#define PIND_2 26
+#define PIND_3 27
+#define PIND_4 28
+#define PIND_5 29
+#define PIND_6 30
+#define PIND_7 31
+
+
+#define PINE_0 32
+#define PINE_1 33
+#define PINE_2 34
+#define PINE_3 35
+#define PINE_4 36
+#define PINE_5 37
+#define PINE_6 38
+#define PINE_7 39
+
+
+#define PINF_0 40
+#define PINF_1 41
+#define PINF_2 42
+#define PINF_3 43
+#define PINF_4 44
+#define PINF_5 45
+#define PINF_6 46
+#define PINF_7 47
+
+
+#define PING_0 48
+#define PING_1 49
+#define PING_2 50
+#define PING_3 51
+#define PING_4 52
+#define PING_5 53
+#define PING_6 54
+#define PING_7 55
+
+
+
+
+
+
+void SetPinDir(u8 PinNumber,u8 PinDirection);
+void SetPinVal(u8 PinNumber,u8 PinValue);
+u8   ReadPinVal(u8 PinNumber);
+
+
+#endif /* DDIO_128_H_ */
